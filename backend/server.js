ac8d3402f2725639b220dcc273cdc7eb26802e36
Tao Zhu <zhutaosx@gmail.com>
2020-10-04 15:07:34 -0700

    add simple backend serving products using array

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const products = require('./products');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('root path in backend');
+});
+
+app.get('/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/products/:id', (req, res) => {
+  res.json(products.find((p) => p._id === req.params.id));
+});
+
+app.listen(5000, () => console.log('backend running'));
