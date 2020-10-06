db5a4c60016c73585acf64756d5cccd1a8f93c22
Tao Zhu <zhutaosx@gmail.com>
2020-10-05 21:59:54 -0700

    add .env

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
 const products = require('./products');
+
+dotenv.config();
 
 const app = express();
 
@@ -18,4 +21,7 @@ app.get('/products/:id', (req, res) => {
   res.json(products.find((p) => p._id === req.params.id));
 });
 
-app.listen(5000, () => console.log('backend running'));
+const PORT = process.env.port || 5000;
+app.listen(PORT, () =>
+  console.log(`backend running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
