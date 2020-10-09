ff74165c2ddc66af3112f6e836f044ab5ae76d78
Tao Zhu <zhutaosx@gmail.com>
2020-10-09 10:22:34 -0700

    Cean up routes by using controller. Add items in product list

--- data/sports_products.js
+++ data/sports_products.js
@@ -1,8 +1,41 @@
 const products = [
   {
+    name: 'Stephen Curry Warriors Icon Edition',
+    image: '/images/curry.jpg',
+    description: `Part of the official uniform of the Golden State Warriors, the Icon Edition represents the team's true colors, reflected in a distinct, instantly recognizable design. This Authentic Nike NBA Jersey features a lightweight, breathable design that's engineered to help basketball's greatest athletes perform on the game's biggest stage.`,
+    brand: 'Nike',
+    category: 'Jerseys',
+    price: 200.0,
+    countInStock: 8,
+    rating: 3.6,
+    numReviews: 9,
+  },
+  {
+    name: 'LeBron James Lakers Icon Edition',
+    image: '/images/james.jpg',
+    description: `The Icon Edition jersey represents the team's true colors, reflected in a distinct, instantly recognizable design. Part of the official Nike NBA uniform, the Los Angeles Lakers Icon Edition Authentic Jersey features lightweight, breathable fabric and a fit that's engineered to let basketball's greatest athletes move freely on the court.`,
+    brand: 'Nike',
+    category: 'Jerseys',
+    price: 200.0,
+    countInStock: 6,
+    rating: 3.4,
+    numReviews: 9,
+  },
+  {
+    name: 'KD13',
+    image: '/images/kd13.jpg',
+    description: `If you want to be a complete player, you have to make an impact on both ends of the floor, from tip-off to the final buzzer. The KD13—built with a full-length Zoom Air unit stitched directly to the upper and an additional Zoom Air unit in the forefoot—provides the responsiveness and optimal energy return to help you make big-time plays all game long.`,
+    brand: 'Nike',
+    category: 'Shoes',
+    price: 150.0,
+    countInStock: 13,
+    rating: 4.2,
+    numReviews: 6,
+  },
+  {
     name:
       'Kobe Bryant Los Angeles Lakers Mitchell & Ness Hardwood Classics 2008-09 Authentic Jersey - Gold',
-    image: '/images/kobe.webp',
+    image: '/images/kobe.jpg',
     description:
       'Through extensive research and attention to detail, Mitchell & Ness Authentics are recreations of the original uniforms worn by the legends of the game. They only use premium fabrics and application as they bring these pieces of history to life. It is an honor to celebrate the history of the game through the uniforms and logos of the icons of sports. Join the celebration with this Los Angeles Lakers Kobe Bryant 2008-09 Hardwood Classics Authentic Jersey.',
     brand: 'Mitchell & Ness',
@@ -15,7 +48,7 @@ const products = [
   {
     name:
       'Dirk Nowitzki Dallas Mavericks Nike Replica Swingman Jersey - Association Edition - White',
-    image: '/images/nowitzki.webp',
+    image: '/images/nowitzki.jpg',
     description:
       "You already cheer the loudest at every Dallas Mavericks game. Take your fandom up a notch and show that you're the ultimate fan when you pick up this Dirk Nowitzki Swingman jersey from Nike. When you wear this authentic jersey while cheering on your team, you stand out as much more than just a fan. Let everyone know you're a true Dallas Mavericks fan with the heart of a champion.",
     brand: 'Nike',
@@ -28,7 +61,7 @@ const products = [
   {
     name:
       'Michael Jordan Chicago Bulls Mitchell & Ness 1997-98 Hardwood Classics Authentic Player Jersey - Red',
-    image: '/images/mj.webp',
+    image: '/images/mj.jpg',
     description: `As a die-hard Chicago Bulls fan, you love to show your appreciation for the storied career of Michael Jordan every chance you get. Now you can pay tribute to your all-time favorite Chicago Bulls star with this 1997-98 Hardwood Classics Authentic Player Jersey from Mitchell & Ness! Its throwback design is constructed to identical standards as the uniform Michael Jordan wore during his unforgettable seasons with the franchise. This jersey's remarkably detailed graphics and high-quality stitching will help bring back all those cherished memories of watching Michael Jordan lead the Chicago Bulls to glory.`,
     brand: 'Mitchell & Ness',
     category: 'Jerseys',
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,35 +1,12 @@
 const express = require('express');
-const asyncHandler = require('express-async-handler');
-const ProductModel = require('../models/productModel');
+const {
+  getProducts,
+  getProductById,
+} = require('../controllers/productController');
 
 const router = express.Router();
 
-// @desc    Fetch all products
-// @route   GET /api/products
-// @access  Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await ProductModel.find({});
-    res.json(products);
-  })
-);
-
-// @desc    Fetch single product
-// @route   GET /api/products/:id
-// @access  Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await ProductModel.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Product not found');
-    }
-  })
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 module.exports = router;
