3a608243f04747122c4471537d85d866ab5b70f5
Tao Zhu <zhutaosx@gmail.com>
2020-10-09 16:49:42 -0700

    Add user registration

--- models/userModel.js
+++ models/userModel.js
@@ -31,6 +31,17 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// Encrypt the plain text password before save
+userSchema.pre('save', async function (next) {
+  // skip if not creating user, just changing name or email
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const UserModel = mongoose.model('User', userSchema);
 
 module.exports = UserModel;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const { authUser, getUserProfile } = require('../controllers/userController');
+const {
+  authUser,
+  registerUser,
+  getUserProfile,
+} = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-router.post('/login', authUser);
+router.route('/login').post(authUser);
+router.route('/').post(registerUser);
 router.route('/profile').get(protect, getUserProfile);
 
 module.exports = router;
