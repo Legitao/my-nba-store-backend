03649891dc3ac96588ffbc7562224c32c5d1aac3
Tao Zhu <zhutaosx@gmail.com>
2020-10-11 11:24:04 -0700

    Change register endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,7 +9,7 @@ const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.route('/login').post(authUser);
-router.route('/').post(registerUser);
+router.route('/register').post(registerUser);
 router.route('/profile').get(protect, getUserProfile);
 
 module.exports = router;
