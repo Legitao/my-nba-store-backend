53dfd65fc9fc9fe25f1613db018c03a6b3923bb3
Tao Zhu <zhutaosx@gmail.com>
2020-10-11 11:52:33 -0700

    Add profile update controller

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ const {
   authUser,
   registerUser,
   getUserProfile,
+  updateUserProfile,
 } = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
 
@@ -10,6 +11,9 @@ const router = express.Router();
 
 router.route('/login').post(authUser);
 router.route('/register').post(registerUser);
-router.route('/profile').get(protect, getUserProfile);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 module.exports = router;
