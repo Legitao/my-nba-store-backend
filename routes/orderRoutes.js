25daf5796f707ffe59883b27ce039269b9020207
Tao Zhu <zhutaosx@gmail.com>
2020-10-12 18:09:48 -0700

    Add update-to-paid endpoint

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,11 +3,13 @@ const express = require('express');
 const {
   addOrderItems,
   getOrderById,
+  updateOrderToPaid,
 } = require('../controllers/orderController');
 const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 router.route('/').post(protect, addOrderItems);
 router.route('/:id').get(protect, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 
 module.exports = router;
