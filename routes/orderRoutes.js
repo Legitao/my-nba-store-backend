e444aff432fc9937c80d53bb5d61e3e473d6a0cc
Tao Zhu <zhutaosx@gmail.com>
2020-10-12 17:40:43 -0700

    Add /orders/:id endpoint

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
-const { addOrderItems } = require('../controllers/orderController');
+const {
+  addOrderItems,
+  getOrderById,
+} = require('../controllers/orderController');
 const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 router.route('/').post(protect, addOrderItems);
+router.route('/:id').get(protect, getOrderById);
 
 module.exports = router;
